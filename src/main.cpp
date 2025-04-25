5eb9d32d81f167d4fc3653a26df0686ef4b007fd
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2025-04-25 09:27:54 -0400

    clean slate done

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/MenuLayer.hpp>
+class $modify(MyMenuLayer, MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init()) {
+			return false;
+		}
+
+		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+
+		auto myButton = CCMenuItemSpriteExtra::create(
+			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+			this,
+			menu_selector(MyMenuLayer::onMyButton)
+		);
+
+		auto menu = this->getChildByID("bottom-menu");
+		menu->addChild(myButton);
+
+		myButton->setID("my-button"_spr);
+
+		menu->updateLayout();
+
+		return true;
+	}
+
+	void onMyButton(CCObject*) {
+		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+	}
+};
